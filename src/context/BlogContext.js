813b5d273c364918589ce010cb05528686ec40a1
xqt19 <tan.xiu.qi.11@gmail.com>
2020-10-31 17:00:29 +0800

    pseudo-redux context refactoring im having some trouble understanding

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,5 @@
 import React, {useState, useReducer} from 'react'
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext'
 
 const blogReducer = (state, action) => {
     switch (action.type){
@@ -11,32 +10,14 @@ const blogReducer = (state, action) => {
     }
 }
 
-export const BlogProvider = ({children}) =>{
-    // USING STATE
-    // const [blogPosts, setBlogPosts] = useState([])
-    // const addBlogPost =()=>{
-    //     setBlogPosts(
-    //         [...blogPosts, 
-    //         {title: `Blog Post #${state.length + 1}`, content: `Lorem Ipsum`}
-    //         ]
-    //     )
-    // }
-
-    // USING REDUCER
-    const [blogPosts, dispatch] = useReducer(blogReducer, [])
-
-    const addBlogPost = () => {
+const addBlogPost = (dispatch) => {
+    return () => {
         dispatch({
             type: 'ADD_BLOGPOST',
             payload: {}
-        })
+        })        
     }
-    
-    return(
-    <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
-        {children}
-    </BlogContext.Provider>
-    )  
+
 }
 
-export default BlogContext
+export const {Context, Provider} = createDataContext(blogReducer, {addBlogPost}, [])
